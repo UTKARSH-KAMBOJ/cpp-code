4fac3cf5ef3e28f375d65fe447a876fcc2764f01
UTKARSH-KAMBOJ <u.kamboj98@gmail.com>
2025-12-23 17:27:07 +0530

    Create sum,sub,multi,div.cpp

---
+++ sum,sub,multi,div.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=0;
+    int b=0;
+    cout<<"enter the first number:"<<endl;
+    cin>>a;
+    cout<<"enter second number:"<<endl;
+    cin>>b;
+    cout<<"your sum is:"<<a+b<<endl;
+    cout<<"your sub is:"<<a-b<<endl;
+    cout<<"your prod is:"<<a*b<<endl;
+    cout<<"yoour div is:"<<a/b<<endl;
+    return 0;
+}
